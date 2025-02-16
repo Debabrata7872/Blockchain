3cd951a2d1e7a8b8410b1b28d08b82cb41a4e7a4
Debabrata Sahoo <debabratasahoo499905@gmail.com>
2025-02-17 01:03:39 +0530

    Footer

--- Weather-App/src/App.jsx
+++ Weather-App/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Weather from './Components/Weather'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
     <div className='app'>
       <Weather/>
+      <Footer/>
     </div>
   )
 }
